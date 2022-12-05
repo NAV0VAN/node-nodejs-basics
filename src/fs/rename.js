f1a17c2ac934d6db96fd0c38c541b483bc51935b
NAV0VAN <nav_van@list.ru>
2022-12-05 17:17:49 +0500

    Adding rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import { rename as fsRename } from "node:fs"
+import { existsSync } from "fs"
+
+
 const rename = async () => {
-    // Write your code here 
+    try {
+        const filePath_1 = './src/fs/files/wrongFilename.txt'
+        const filePath_2 = './src/fs/files/properFilename.md'
+        if (!existsSync(filePath_1) || existsSync(filePath_2)) {
+            throw "FS operation failed (existsSync)"
+        }
+        fsRename(filePath_1, filePath_2, (err) => {
+            if (err) throw "Rename failed!";
+            console.log('Rename complete!');
+        });
+    } catch (error) {
+        console.error(error)
+    }
 };
 
 await rename();
