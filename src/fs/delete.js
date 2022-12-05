233535d4f3fc0701f7ccd09a644099a7d1e11b93
NAV0VAN <nav_van@list.ru>
2022-12-05 17:40:34 +0500

    Adding delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,19 @@
-const remove = async () => {
-    // Write your code here 
+import { existsSync, unlink } from "node:fs"
+
+const delFilePath = './src/fs/files/fileToRemove.txt'
+
+const remove = async (delFilePath) => {
+    try {
+        if (existsSync(delFilePath)) {
+            unlink(delFilePath, (err) => {
+                if (err) throw "Delete operation failed";
+            })
+        } else {
+            throw "FS operation failed (file is missing)"
+        }
+    } catch (error) {
+        console.error("ERROR: ", error)
+    }
 };
 
-await remove();
+await remove(delFilePath);
