a02801aff71408611e5ad7e972c3880aad3e1843
NAV0VAN <nav_van@list.ru>
2022-12-05 18:47:04 +0500

    Adding read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
-const read = async () => {
-    // Write your code here 
+import { existsSync } from 'node:fs';
+import { readFile } from 'node:fs/promises';
+
+const filePath = './src/fs/files/fileToRead.txt'
+
+const read = async (filePath) => {
+    try {
+        if (existsSync(filePath)) {
+            const contents = await readFile(filePath, { encoding: 'utf8' });
+            console.log(contents);
+        } else {
+            throw "FS operation failed (file is missing)"
+        }
+    } catch (err) {
+        console.error("ERROR: ", err);
+    }
 };
 
-await read();
+await read(filePath);
