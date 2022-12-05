7c1e66349735ebfe7cb219e53a5bd1ac77ce7122
NAV0VAN <nav_van@list.ru>
2022-12-05 17:57:49 +0500

    Adding list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
-const list = async () => {
-    // Write your code here 
+import { existsSync } from "fs";
+import { readdir } from "fs/promises"
+
+const folderPath = './src/fs/files'
+
+const list = async (folderPath) => {
+    try {
+        if (existsSync(folderPath)) {
+            const files = await readdir(folderPath);
+            for (const file of files)
+                console.log(file);
+        } else {
+            throw "FS operation failed (directory is missing)"
+        }
+    } catch (err) {
+        console.error("ERROR: ", err);
+    }
 };
 
-await list();
+await list(folderPath);
