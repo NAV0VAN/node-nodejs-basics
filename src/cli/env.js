74631d62a89c6d342a77d191bc520907e10bef8e
NAV0VAN <nav_van@list.ru>
2022-12-05 23:17:16 +0500

    Adding env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,17 @@
+// реализовать функцию, которая анализирует переменные окружения с префиксом RSS_ 
+// и выводит их на консоль в формате RSS_name1=value1; RSS_name2=value2
+
 const parseEnv = () => {
-    // Write your code here 
+    try {
+        const envVariables = process.env
+        for (let nameVar in envVariables) {
+            if (nameVar.startsWith("RSS_")) {
+                console.log(nameVar, " = ", envVariables[nameVar])
+            }
+        }
+    } catch (error) {
+        console.log("ERROR: ", error)
+    }
 };
 
 parseEnv();
