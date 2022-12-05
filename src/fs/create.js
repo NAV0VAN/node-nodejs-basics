9e4f556d9c64f0378fecfc1eb1e28786cd64e404
NAV0VAN <nav_van@list.ru>
2022-12-05 10:32:15 +0500

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import * as fs from 'node:fs/promises';
+import { existsSync } from 'node:fs';
+
 const create = async () => {
-    // Write your code here 
-};
+    try {
+        if (existsSync("./src/fs/files/fresh.txt")) {
+            throw "FS operation failed"
+        } else {
+            fs.writeFile("./src/fs/files/fresh.txt", "I am fresh and young").then((err) => {
+                if (err) {
+                    console.log("ERROR in  fs.writeFile", err);
+                } else {
+                    console.log("Succsess, file create");
+                }
+            })
+        }
+    } catch (error) {
+        console.log("ERROR in create() ", error)
+    }
+}
 
 await create();
