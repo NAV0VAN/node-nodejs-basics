7a3c0f1c74b819d54c2dbb43716ff490dbc51968
NAV0VAN <nav_van@list.ru>
2022-12-06 07:51:17 +0500

    Adding args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
+/**
+ * Функция, которая анализирует аргументы командной строки (данные в формате --propName value --prop2Name value2) 
+ * и выводит их в консоль в формате: propName is value, prop2Name is value2
+ */
+
 const parseArgs = () => {
-    // Write your code here 
+    try {
+        const argsList = process.argv
+        argsList.forEach((el, index) => {
+            el.startsWith('--') ? console.log(`${el.slice(2)} is ${argsList[index + 1]}`) : null
+        })
+    } catch (error) {
+        console.log("ERROR:", error)
+    }
+
+
 };
 
 parseArgs();
