8bfb1bf5835744dea4bbfc26c74bebfe68f7c8c3
NAV0VAN <nav_van@list.ru>
2022-12-07 11:16:00 +0500

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,12 +1,17 @@
 import * as fs from 'node:fs/promises';
 import { DIR_PATH, ERROR_MSG, SUCCESS_MSG } from './constants.js'
+import * as path from 'path'
 
 const FILE_NAME = 'fresh.txt'
+const FOLDER_NAME = 'files'
 const FILE_CONTENT = '"I am fresh and young"'
+
+const FULL_PATH = path.join(DIR_PATH, FOLDER_NAME, FILE_NAME)
+
 
 const create = async () => {
     try {
-        await fs.writeFile(`${DIR_PATH}/files/${FILE_NAME}`, FILE_CONTENT, { flag: 'wx' })
+        await fs.writeFile(FULL_PATH, FILE_CONTENT, { flag: 'wx' })
             .then(() => {
                 console.info(SUCCESS_MSG)
             })
