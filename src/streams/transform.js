ed526a1c01881f52d42b422538625d594ec2766b
NAV0VAN <nav_van@list.ru>
2022-12-08 16:48:33 +0500

    feat: add transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,22 @@
+// Функция, которая считывает строку из process.stdin, переворачивает текст с помощью Transform Stream 
+// и затем записывает его вprocess.stdout
+
+import { stdin, stdout } from 'node:process';
+import { Transform } from 'node:stream';
+import { pipeline } from 'node:stream/promises';
+
+const reversStr = (str) =>
+    str.toString().split("")
+        .reverse()
+        .join("")
+
 const transform = async () => {
-    // Write your code here 
+    const myTransform = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, reversStr(chunk))
+        }
+    })
+    await pipeline(stdin, myTransform, stdout)
 };
 
 await transform();
